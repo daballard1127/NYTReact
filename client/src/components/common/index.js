30d04b816fc0bdfa520951313d34bd17ed73a0a3
daballard1127 <denise_ballard@att.net>
2018-02-24 12:27:41 -0600

    Homework Add

---
+++ client/src/components/common/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Footer: require("./Footer"),
+    Results: require("./Results"),
+    Saved: require("./Saved"),
+    Search: require("./Search"),
+
+  };
+  
---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // Gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
+  },
+  // Gets the book with the given id
+  getArticles: function(id) {
+    return axios.get("/api/articles/" + id);
+  },
+  // Deletes the book with the given id
+  deleteArticles: function(id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  // Saves a book to the database
+  getsaveArticles: function(bookData) {
+    return axios.post("/api/article", articleData);
+  }
+};
